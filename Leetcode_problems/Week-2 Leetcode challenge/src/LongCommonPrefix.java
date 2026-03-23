0ea8407d00be488a5b1c02aec6e188394d49effa
Ajay-4772 <ajay.m180405@gmail.com>
2026-03-23 23:32:45 +0530

    Week 2 Leetcode challenge -Day 1 14- Longest common prefix

---
+++ Leetcode_problems/Week-2 Leetcode challenge/src/LongCommonPrefix.java
@@ -0,0 +1,24 @@
+
+public class LongCommonPrefix{
+    public static void main(String[] args) {
+        String[] arr = {"flower" , "flow" , "flight"};
+        String[] arr2 = {"dog","racecar","car"};
+
+        System.out.println(longestCommonPrefix(arr));
+        System.out.println(longestCommonPrefix(arr2));
+
+    }
+    public static String longestCommonPrefix(String[] strs) {
+        String s = "";
+        for(int i=0;i<strs[0].length();i++){
+            char a = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i>=strs[j].length() || strs[j].charAt(i) != a) {
+                    return s;
+                }
+            }
+            s+=a;
+        }
+        return s;
+    }
+}
