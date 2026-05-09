19fc2aa333f083e58237e76560af30b22f8de34b
Ajay-4772 <ajay.m180405@gmail.com>
2026-05-09 22:36:25 +0530

    Day 6 Leetcode problem 628 Maximum product of three numbers

---
+++ Java_100_days_challenge/src/MaxProductOfThreeNumbers.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class MaxProductOfThreeNumbers {
+    public static int maximumProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for(int num:nums){
+            if(num>max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            }
+            else if(num>max2){
+                max3 = max2;
+                max2 = num;
+            }
+            else if(num>max3){
+                max3 = num;
+            }
+            if(num<min1){
+                min2 = min1;
+                min1 = num;
+            }
+            else if(num<min2){
+                min2 = num;
+            }
+        }
+        int p1 = max1*max2*max3;
+        int p2 = min1*min2*max1;
+
+        return Math.max(p1,p2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(maximumProduct(arr));
+    }
+}
