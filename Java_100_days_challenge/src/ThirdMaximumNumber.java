626bb6f3c0bbabcc888f0282c85cc56ccae4000c
Ajay-4772 <ajay.m180405@gmail.com>
2026-05-25 20:18:46 +0530

    414. Third Maximum Number

---
+++ Java_100_days_challenge/src/ThirdMaximumNumber.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class ThirdMaximumNumber {
+
+    public static int thirdMax(int[] nums) {
+        Long first = null;
+        Long second = null;
+        Long third = null;
+
+        for (int num : nums) {
+
+            if ((first != null && num == first) ||
+                    (second != null && num == second) ||
+                    (third != null && num == third)) {
+                continue;
+            }
+
+            if (first == null || num > first) {
+                third = second;
+                second = first;
+                first = (long) num;
+            }
+            else if (second == null || num > second) {
+                third = second;
+                second = (long) num;
+            }
+            else if (third == null || num > third) {
+                third = (long) num;
+            }
+        }
+
+        return third == null ? first.intValue() : third.intValue();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int result = thirdMax(nums);
+
+        System.out.println(result);
+
+    }
+}
